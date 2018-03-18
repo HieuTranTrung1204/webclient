258958cca25d00edf92ea262518fb6bea01a085c
trungnd2 <trungnd2@vng.com.vn>
2018-03-18 16:40:20 +0700

    add feature view image

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( "img" ).click(function(e) {
+    e.preventDefault();
+    $('.tr-viewimg #ct').attr('src', $(this).attr("src"));
+    $('.tr-viewimg').show();
+});
+
+$(".btnclose").click(function(){
+    $('.tr-viewimg').hide();
+})
